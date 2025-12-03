bb2b29e6b5511fa993d3b832b2a9d94f0867f974
naikkuma <carlos.escom.ipn@gmail.com>
2025-12-02 19:15:49 -0600

    feat: +tryExecutingOrders

--- src/TakeProfitsHook.sol
+++ src/TakeProfitsHook.sol
@@ -294,4 +294,87 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
         // i.e. -2 * 60 = -120
         return intervals * tickSpacing;
     }
+
+    function tryExecutingOrders(
+        PoolKey calldata key,
+        bool executeZeroForOne
+    ) internal returns (bool tryMore, int24 newTick) {
+        (, int24 currentTick, , ) = poolManager.getSlot0(key.toId());
+        int24 lastTick = lastTicks[key.toId()];
+
+        // Given `currentTick` and `lastTick`, 2 cases are possible:
+
+        // Case (1) - Tick has increased, i.e. `currentTick > lastTick`
+        // or, Case (2) - Tick has decreased, i.e. `currentTick < lastTick`
+
+        // If tick increases => Token 0 price has increased
+        // => We should check if we have orders looking to sell Token 0
+        // i.e. orders with zeroForOne = true
+        // i.e. Token 0 price has increased
+
+        // e.g. in an ETH/USDC pool, people are buying ETH for USDC causing ETH price to increase
+        // We should check if we have any orders looking to sell Token 0
+        // at ticks `lastTick` to `currentTick`
+        // i.e. check if we have any orders to sell ETH at the new price that ETH is at now because of the increase
+        if (currentTick > lastTick) {
+            // Loop over all ticks from `lastTick` to `currentTick`
+            // and execute orders that are looking to sell Token 0
+            for (
+                int24 tick = lastTick;
+                tick < currentTick;
+                tick += key.tickSpacing
+            ) {
+                uint256 inputAmount = pendingOrders[key.toId()][tick][
+                    executeZeroForOne
+                ];
+                if (inputAmount > 0) {
+                    // An order with these parameters can be placed by one or more users
+                    // We execute the full order as a single swap
+                    // Regardless of how many unique users placed the same order
+                    executeOrder(key, tick, executeZeroForOne, inputAmount);
+
+                    // Return true because we may have more orders to execute
+                    // from lastTick to new current tick
+                    // But we need to iterate again from scratch since our sale of ETH shifted the tick down
+                    return (true, currentTick);
+                }
+            }
+        }
+
+        // If tick decreases => Token 1 price has increased
+        // => We should check if we have orders looking to sell Token 1
+        // i.e. orders with zeroForOne = false
+
+        // Tick has increased i.e. people bought Token 0 by selling Token 1
+        // ------------
+        // Case (2)
+        // ------------
+        // Tick has gone down i.e. people bought Token 1 by selling Token 0
+        // i.e. Token 1 price has increased
+        // e.g. in an ETH/USDC pool, people are selling ETH for USDC causing ETH price to decrease (and USDC to increase)
+        // We should check if we have any orders looking to sell Token 1
+        // at ticks `currentTick` to `lastTick`
+        // i.e. check if we have any orders to buy ETH at the new price that ETH is at now because of the decrease
+        else {
+            for (
+                int24 tick = lastTick;
+                tick > currentTick;
+                tick -= key.tickSpacing
+            ) {
+                uint256 inputAmount = pendingOrders[key.toId()][tick][
+                    executeZeroForOne
+                ];
+                if (inputAmount > 0) {
+                    executeOrder(key, tick, executeZeroForOne, inputAmount);
+                    return (true, currentTick);
+                }
+            }
+        }
+
+        // ------
+
+        // If no orders were found to be executed, we don't need to try
+        // executing any more - return `false` and `currentTick`
+        return (false, currentTick);
+    }
 }
